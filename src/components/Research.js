e6bc0b6c49ee5509665e745833a65fdc6124f43d
sz7364 <sz7364@g.rit.edu>
2024-04-25 10:29:46 -0400

    no2 research added + typical treatments

--- src/components/Research.js
+++ src/components/Research.js
@@ -4,7 +4,62 @@ function Research() {
     return (
         <div>
           <h1>Research</h1>
-          <p>n/a</p>
+          <h3>Asthma as a result of Nitrogen Dioxide</h3>
+          <p>
+          “Nitrogen dioxide (NO2) is a toxic gas, a product of combustion in malfunctioning ice-resurfacing machines. 
+          NO2 poisoning is rare but potentially lethal” (Bret et al., 2013). High concentrations of nitrogen dioxide 
+          have been measured in arenas because of the combustion engine-powered machines that re-ice the floors and 
+          keep the arena cold. Exposure to NO2 in ice arenas is associated with an increased prevalence of asthma symptoms 
+          among athletes. 
+          </p>
+
+          <p>
+          The toxic nature of NO2 contributes to its harmful effects on the respiratory system. NO2 easily penetrates the 
+          peripheral airways and alveoli, where it reacts with water to form nitrous and nitric acid. These acids can increase 
+          cell membrane permeability, leading to pulmonary edema and pneumonitis, exacerbating respiratory conditions like asthma.
+          </p>
+
+          <p>
+          To mitigate the risks associated with NO2 exposure in ice arenas, various strategies have been proposed. One approach 
+          is to replace combustion engine-powered ice-resurfacing machines with electrically powered ones, which produce lower 
+          levels of NO2 emissions. Additionally, regular environmental assessments and service controls should be conducted to 
+          ensure that NO2 levels are kept within safe limits.
+          </p>
+
+          <h3>
+            Typical Treatments for Asthma
+          </h3>
+          <p>
+          Although asthma isn’t a condition that can be fixed, there are therapies to help with the symptoms. Most athletes 
+          are prescribed drugs like Short-Acting Beta Agonists (SABAs). SABAs are inhaled medicines that help open airways. 
+          It relaxes the bronchial smooth muscle, treating bronchospasm that is refractory to the epinephrine. SABAs are typically 
+          taken 15-30 minutes before exercising. If the athlete doesn’t want to go the drug route, they can also supplement their 
+          inhaler therapy with a few other options:
+          </p>
+
+          <h4>
+          Heat and Moisture Face Masks
+          </h4>
+          <p>
+          “Face masks worn during training and prior to competition are able to capture the heat and moisture in exhaled breath 
+          and use it to warm and moisten the inhaled air as a defense against airway dehydration and subsequent narrowing”
+          </p>
+
+          <h4>
+            Fish Oils
+          </h4>
+          <p>
+          “Studies suggest that the use of omega-3 fish oils can reduce airway inflammation and provide a protective effect 
+          against asthma associated with exercise.”
+          </p>
+
+          <h4>
+            Prebiotics
+          </h4>
+          <p>
+          “Recent evidence suggests that a dietary prebiotic supplement that targets the good bacteria in the gut can 
+          reduce the severity of asthma in physically active asthma patients and reduce airway inflammation.”
+          </p>
         </div>
       );
 }
